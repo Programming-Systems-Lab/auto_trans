86672502613f4c7265c9e8a361b9a211c5ad83f2
Fang-hsiang Su <fs2455@columbia.edu>
2017-06-29 17:07:09 -0400

    Add comments and steps for implementation

--- scripts/dataParse.py
+++ scripts/dataParse.py
@@ -7,10 +7,20 @@ def downloader(url, path):
     html = urllib2.urlopen(url)
     soup = BeautifulSoup(html, "html.parser")
     links = []
+
+    #The way to identify if a href is downloadable is not rigorous
+    #Need to analyze the title attribute
+    #link.get('title').startswith('Download solution by')
     for link in soup.find_all('a'):
         if link.get('href')[:7] == "http://":
             links.append(link.get('href'))
-
+    
+    #The current naming is not meaningful
+    #Use year-round-problem-id-username-language to form the file name
+    #This can be done by analyzing the url and the title attribute
+    #Instead of using list to contain all links, use a dict
+    #links = dict(); links[key] = link
+    #key will be the file name and the value is the link
     for i in range(len(links)):
         print "Downloading: %s/%s" % (i + 1, len(links))
         temp = str(links[i])
