b00f57e6c60e633b7b94bb518512b71ce53e43ca
Fang-hsiang Su <fs2455@columbia.edu>
2017-08-22 11:42:23 -0400

    reorg test

--- test_example.py
+++ test_example.py
@@ -1,10 +1,11 @@
 import sys
+import os
 import unittest
-import example
+import test_example.example
 
 class TestExample(unittest.TestCase):
   def test_add(self):
-    self.assertEqual(example.add(3, 5), 8)
+    self.assertEqual(test_example.example.add(3, 5), 8)
 
 if __name__ == '__main__':
   unittest.main()
