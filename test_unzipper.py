e0fc0f6d3f71d21963e2ac6ff292b91006de4731
Winston Yang <wyang737@users.noreply.github.com>
2017-09-04 22:39:20 -0400

    pathing

--- test_unzipper.py
+++ test_unzipper.py
@@ -4,14 +4,14 @@ import unittest
 
 class TestZipper(unittest.TestCase):
     def test_zipper_successful(self):
-        for file in os.listdir("./2014-Round. 3-1-argentoy-Java.zip"):
+        for file in os.listdir("./2014-Round 3-1-argentoy-Java.zip"):
             if file.endswith(".zip"):
-                zip_ref = zipfile.ZipFile("%s/%s" % ("./2014-Round. 3-1-argentoy-Java.zip", file), 'r')
-                zip_ref.extractall("%s/%s" % ("./2014-Round. 3-1-argentoy-Java.zip", file[:len(file) - 4]))
+                zip_ref = zipfile.ZipFile("%s/%s" % ("./2014-Round 3-1-argentoy-Java.zip", file), 'r')
+                zip_ref.extractall("%s/%s" % ("./2014-Round 3-1-argentoy-Java.zip", file[:len(file) - 4]))
                 zip_ref.close()
-                os.remove("%s/%s" % ("./2014-Round. 3-1-argentoy-Java.zip", file))
+                os.remove("%s/%s" % ("./2014-Round 3-1-argentoy-Java.zip", file))
         count = 0
-        for file in os.listdir("./2014-Round. 3-1-argentoy-Java.zip"):
+        for file in os.listdir("./2014-Round 3-1-argentoy-Java.zip"):
             if file.endswith(".zip"):
                 count += 1
         self.assertEqual(count, 0)
