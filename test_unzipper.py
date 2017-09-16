a99bed5bad0bae3d08224c47555e19e4a684be00
Winston Yang <wyang737@users.noreply.github.com>
2017-09-16 07:54:16 -0400

    Update test_unzipper.py

--- test_unzipper.py
+++ test_unzipper.py
@@ -6,7 +6,7 @@ class TestZipper(unittest.TestCase):
     def test_zipper_successful(self):
         for file in os.listdir("./"):
             if file.endswith(".zip"):
-                zip_ref = zipfile.ZipFile("%s/%s" % ("./2014-Round 3-1-argentony-Java.zip", file), 'r')
+                zip_ref = zipfile.ZipFile("%s/%s" % ("./", file), 'r')
                 zip_ref.extractall("%s/%s" % ("./2014-Round 3-1-argentony-Java.zip", file[:len(file) - 4]))
                 zip_ref.close()
                 os.remove("%s/%s" % ("./2014-Round 3-1-argentony-Java.zip", file))
