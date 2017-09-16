ab991f8686b645b68c3712273b177ed2658988c9
Winston Yang <wyang737@users.noreply.github.com>
2017-09-16 07:51:52 -0400

    path

--- test_unzipper.py
+++ test_unzipper.py
@@ -4,7 +4,7 @@ import unittest
 
 class TestZipper(unittest.TestCase):
     def test_zipper_successful(self):
-        for file in os.listdir("./2014-Round 3-1-argentony-Java.zip"):
+        for file in os.listdir("./"):
             if file.endswith(".zip"):
                 zip_ref = zipfile.ZipFile("%s/%s" % ("./2014-Round 3-1-argentony-Java.zip", file), 'r')
                 zip_ref.extractall("%s/%s" % ("./2014-Round 3-1-argentony-Java.zip", file[:len(file) - 4]))
